c1bf42c45e1d920c6781d0129d8088711ad72bf6
Poncho <ameneses67@gmail.com>
2023-04-08 16:54:42 -0600

    migración a async/await

--- src/scripts/buscarImagenes.ts
+++ src/scripts/buscarImagenes.ts
@@ -15,19 +15,29 @@ export interface Imagenes {
   webformatURL: string;
 }
 
-export function buscarImagenes() {
+export async function buscarImagenes() {
   const terminoBusqueda = inputBusqueda.value;
 
   const key = "8726170-5d58a16fba3491d35dafe5c15";
   const url = `https://pixabay.com/api/?key=${key}&q=${terminoBusqueda}&image_type=photo&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-  fetch(url)
-    .then(
-      (respuesta): Promise<{ hits: Imagenes[]; totalHits: number }> =>
-        respuesta.json()
-    )
-    .then((resultado) => {
-      totalPaginas = calcularPaginas(resultado.totalHits);
-      mostrarImagenes(resultado.hits);
-    });
+  // fetch(url)
+  //   .then(
+  //     (respuesta): Promise<{ hits: Imagenes[]; totalHits: number }> =>
+  //       respuesta.json()
+  //   )
+  //   .then((resultado) => {
+  //     totalPaginas = calcularPaginas(resultado.totalHits);
+  //     mostrarImagenes(resultado.hits);
+  //   });
+
+  //async await
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    totalPaginas = calcularPaginas(resultado.totalHits);
+    mostrarImagenes(resultado.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
